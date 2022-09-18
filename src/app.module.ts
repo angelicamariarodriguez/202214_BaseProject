88c8b9d98d286c96e3a8773df0ceec725a3203d7
Angelica Rodriguez <am.rodriguez12@uniandes.edu.co>
2022-09-18 12:38:45 -0500

    Se crea persistencia de Producto y Tienda

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductoEntity } from './producto/producto.entity';
+import { ProductoModule } from './producto/producto.module';
+import { TiendaEntity } from './tienda/tienda.entity';
+import { TiendaModule } from './tienda/tienda.module';
 
 @Module({
-  imports: [],
+  imports: [TiendaModule, ProductoModule, 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'tiendas',
+      entities: [TiendaEntity, ProductoEntity],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
-})
-export class AppModule {}
+ })
+ export class AppModule {}
---
+++ src/tienda/tienda.entity.ts
@@ -0,0 +1,29 @@
+
+import { ProductoEntity } from '../producto/producto.entity';
+import {
+    Column,
+    Entity,
+    JoinTable,
+    ManyToMany,
+    PrimaryGeneratedColumn,
+  } from 'typeorm';
+
+  
+  @Entity()
+  export class TiendaEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+  
+    @Column()
+    nombre: string;
+  
+    @Column()
+    ciudad: string;
+  
+    @Column()
+    direccion: string;
+  
+    @ManyToMany(() => ProductoEntity, (producto) => producto.tiendas)
+    @JoinTable()
+    productos: ProductoEntity[];
+  }
